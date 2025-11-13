c083b823e8214fbf536ba262e04fab2f547ae1b2
Aathisankar A <aathisankarayyappan@gmail.com>
2025-11-13 18:52:38 +0530

    commit

---
+++ Leetcode/Nov_2025/13_11_25.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxOperations(string s) {
+        int n = s.size();
+
+        int cnt = 0;
+        int res = 0;
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == '1'){
+                cnt++;
+            }
+            else if(i > 0 && s[i-1] == '1'){
+                res += cnt;
+            }
+        }
+        return res;
+    }
+};
