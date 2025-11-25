4d1871296d10dbfc3907f2e9255ed7f7d2cf12af
Aathisankar A <aathisankarayyappan@gmail.com>
2025-11-25 15:53:27 +0530

    commit

---
+++ GFG/Nov_2025/25_11_25.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int subarrayXor(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+            if(n % 2 == 0) return 0;
+    
+            int res = 0;
+            for(int i = 0; i < n; i += 2){
+                res ^= arr[i];
+            }
+            return res;
+    }
+};
