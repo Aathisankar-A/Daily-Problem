93f76b9fd9cdff9fc533385c554850180a20285f
Aathisankar A <aathisankarayyappan@gmail.com>
2025-12-08 12:47:28 +0530

    commit

---
+++ GFG/DEC_2025/08_12_25.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    string build(int i, int j, vector<vector<int>> &split){
+        if(i == j){
+            char c = 'A' + (i - 1);
+            return string(1, c);
+        }
+        int k = split[i][j];
+        string left = build(i, k, split);
+        string right = build(k + 1, j, split);
+        return "(" + left + right + ")";
+    }
+
+    string matrixChainOrder(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        int m = n - 1;
+    
+        vector<vector<int>> dp(m + 1, vector<int>(m + 1, 0));
+        vector<vector<int>> split(m + 1, vector<int>(m + 1, 0));
+    
+        for(int len = 2; len <= m; len++){
+            for(int i = 1; i + len - 1 <= m; i++){
+                int j = i + len - 1;
+                dp[i][j] = INT_MAX;
+    
+                for(int k = i; k < j; k++){
+                    int cost = dp[i][k] + dp[k + 1][j] + arr[i - 1] * arr[k] * arr[j];
+                    if(cost < dp[i][j]){
+                        dp[i][j] = cost;
+                        split[i][j] = k;
+                    }
+                }
+            }
+        }
+    
+        return build(1, m, split);
+    }
+};
