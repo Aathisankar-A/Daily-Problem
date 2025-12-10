3e50f85eb18e47509bd5dd151062b9084c057066
Aathisankar A <aathisankarayyappan@gmail.com>
2025-12-10 18:25:19 +0530

    commit

---
+++ Leetcode/10_12_25.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countPermutations(vector<int>& complexity) {
+        int n = complexity.size();
+        if(*min_element(complexity.begin() + 1, complexity.end()) <= complexity[0]){
+            return 0;
+        }
+
+        int mod = 1000000007;
+        int res = 1;
+        for(int i = 2; i < n; ++i){
+            res = (long long)(res) * i % mod;
+        }
+        return res;
+    }
+};
