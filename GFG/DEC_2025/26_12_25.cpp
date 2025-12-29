65bc1ded31a78c388f3b466743b379020de519c4
Aathisankar A <aathisankarayyappan@gmail.com>
2025-12-29 20:16:15 +0530

    commit

---
+++ GFG/DEC_2025/26_12_25.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int kthMissing(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        int l = 0;
+        int r = n - 1;
+
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            int miss = arr[mid] - (mid + 1);
+
+            if(miss < k)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+
+        return k + l;
+    }
+};
