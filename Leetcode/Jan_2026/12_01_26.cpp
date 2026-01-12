0729321491a3782dea7832a771608cac79d6094c
Aathisankar A <aathisankarayyappan@gmail.com>
2026-01-12 22:13:48 +0530

    commit

---
+++ Leetcode/Jan_2026/12_01_26.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        int res = 0;
+
+        for(int i = 1; i < n; i++){
+            int dx = abs(points[i-1][0] - points[i][0]);
+            int dy = abs(points[i-1][1] - points[i][1]);
+
+            res += max(dx, dy);
+        }
+        return res;
+    }
+};
