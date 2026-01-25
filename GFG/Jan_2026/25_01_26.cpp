7a291b06388a52eafbc50ca7a7127459a0f644e8
Aathisankar A <aathisankarayyappan@gmail.com>
2026-01-25 21:16:04 +0530

    commit

---
+++ GFG/Jan_2026/25_01_26.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int findWays(int n) {
+        // code here
+        if(n % 2 != 0)
+            return 0;
+
+        int k = n / 2;
+        vector<int> dp(k + 1, 0);
+        dp[0] = 1;
+
+        for(int i = 1; i <= k; i++){
+            for(int j = 0; j < i; j++){
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+        return dp[k];
+    }
+};
