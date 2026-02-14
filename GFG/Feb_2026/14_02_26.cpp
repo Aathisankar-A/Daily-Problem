406a2a228fc62841dbe441886b56c0c28e762af5
Aathisankar A <aathisankarayyappan@gmail.com>
2026-02-14 20:41:04 +0530

    commit

---
+++ GFG/Feb_2026/14_02_26.cpp
@@ -0,0 +1,47 @@
+class Solution {
+  public:
+    bool canPaint(vector<int>& arr, int k, long long maxTime){
+        long long sum = 0;
+        int painters = 1;
+        
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] > maxTime) return false;
+            
+            if(sum + arr[i] <= maxTime){
+                sum += arr[i];
+            }
+            else{
+                painters++;
+                sum = arr[i];
+            }
+        }
+        
+        return painters <= k;
+    }
+    
+    int minTime(vector<int>& arr, int k) {
+        // code here
+        long long low = 0, high = 0;
+        
+        for(int i = 0; i < arr.size(); i++){
+            low = max(low, (long long)arr[i]);
+            high += arr[i];
+        }
+        
+        long long ans = high;
+        
+        while(low <= high){
+            long long mid = low + (high - low) / 2;
+            
+            if(canPaint(arr, k, mid)){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
