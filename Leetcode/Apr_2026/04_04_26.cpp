db2ea742564ae8f8f73c6e6f317ce1515921dfa6
Aathisankar A <aathisankarayyappan@gmail.com>
2026-04-04 17:20:16 +0530

    commit

---
+++ Leetcode/Apr_2026/04_04_26.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        int n = encodedText.size();
+        
+        if(rows == 1)
+            return encodedText;
+        
+        int cols = n / rows;
+        string res;
+        res.reserve(n);
+        
+        for(int c = 0; c < cols; c++){
+            int r = 0;
+            int j = c;
+            
+            while(r < rows && j < cols){
+                res += encodedText[r * cols + j];
+                r++;
+                j++;
+            }
+        }
+        
+        while(!res.empty() && res.back() == ' ')
+            res.pop_back();
+        
+        return res;
+    }
+};
