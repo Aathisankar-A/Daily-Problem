bcaf59e648fd56fdd2cf87e3fdb95bf5056414b6
Aathisankar A <aathisankarayyappan@gmail.com>
2026-04-17 19:10:15 +0530

    commit

---
+++ GFG/Apr_2026/17_04_26.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    bool canFormPalindrome(string &s) {
+        // code here
+        unordered_map<char, int> freq;
+
+        for(char c : s){
+            freq[c]++;
+        }
+    
+        int oddCount = 0;
+    
+        for(auto it : freq){
+            if(it.second % 2 != 0){
+                oddCount++;
+            }
+        }
+    
+        if(oddCount <= 1)
+            return true;
+        else
+            return false;
+    }
+};
