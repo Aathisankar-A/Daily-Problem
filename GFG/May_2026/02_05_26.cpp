bcdb904f039d455758dcdcb03839304305f33ef4
Aathisankar A <aathisankarayyappan@gmail.com>
2026-05-05 19:52:28 +0530

    commit

---
+++ GFG/May_2026/02_05_26.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int findPosition(int n) {
+        // code here
+        if(n == 0) return -1;
+
+        if((n & (n - 1)) != 0) return -1;
+
+        int pos = 1;
+
+        while(n > 1){
+            n = n >> 1;
+            pos++;
+        }
+
+        return pos;
+    }
+};
