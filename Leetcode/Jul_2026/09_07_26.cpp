989b42b34d5f27d9004dc9c01cc0213734cc139e
Aathisankar A <aathisankarayyappan@gmail.com>
2026-07-09 19:40:28 +0530

    commit

---
+++ Leetcode/Jul_2026/09_07_26.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<bool> ans;
+
+        vector<int> comp(n, 0);
+        int id = 0;
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] - nums[i-1] > maxDiff){
+                id++;
+            }
+            comp[i] = id;
+        }
+
+        for(int i = 0; i < queries.size(); i++){
+            int u = queries[i][0];
+            int v = queries[i][1];
+
+            ans.push_back(comp[u]==comp[v]);
+        }
+        return ans;
+    }
+};
