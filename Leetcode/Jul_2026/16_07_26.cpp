662da89add1aa1fbe3d7e3df8c410dc46c06ad09
Aathisankar A <aathisankarayyappan@gmail.com>
2026-07-16 21:47:34 +0530

    commit

---
+++ Leetcode/Jul_2026/16_07_26.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+        int n = nums.size();
+        int mx = INT_MIN;
+
+        for(int i = 0; i < n; i++){
+            mx = max(mx, nums[i]);
+
+            nums[i] = gcd(nums[i], mx);
+        }
+
+        sort(nums.begin(), nums.end());
+
+        int i = 0;
+        int j = n-1;
+
+        long long sum = 0;
+
+        while(i < j){
+            sum = sum + gcd(nums[i], nums[j]);
+
+            i++;
+            j--;
+        }
+
+        return sum;
+    }
+};
