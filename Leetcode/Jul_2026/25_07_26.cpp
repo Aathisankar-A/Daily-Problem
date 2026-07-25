d3c314192303ed0fb46b51080e3ac5534ee0fe1f
Aathisankar A <aathisankarayyappan@gmail.com>
2026-07-25 21:13:34 +0530

    commit

---
+++ Leetcode/Jul_2026/25_07_26.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int f = 0;
+        int s = 0;
+
+        while(n > 0){
+            int x = n % 10;
+            if(x > s){
+                s = f;
+                f = x;
+            }
+            else if(x > s){
+                s = x;
+            }
+            n /= 10;
+        }
+        return f * s;
+    }
+};
