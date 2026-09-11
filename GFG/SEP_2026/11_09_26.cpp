1b7997b46bbf5b3b30d0f17c559b4006421404c7
Aathisankar A <aathisankarayyappan@gmail.com>
2026-09-11 19:20:37 +0530

    commit

---
+++ GFG/SEP_2026/11_09_26.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int sameMod(vector<int> &arr) {
+        // code here
+        int g = 0;
+
+        for(int i = 1; i < arr.size(); i++){
+            g = gcd(g, abs(arr[i] - arr[0]));
+        }
+
+        if(g == 0){
+            return -1;
+        }
+
+        int cnt = 0;
+
+        for(int i = 1; i * i <= g; i++){
+            if(g % i == 0){
+                cnt++;
+
+                if(i != g / i){
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
