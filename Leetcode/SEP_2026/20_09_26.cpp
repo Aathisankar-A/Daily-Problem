5444d6043d6c2cd7cfd7515bc51e9ad366d105fa
Aathisankar A <aathisankarayyappan@gmail.com>
2026-09-20 21:13:01 +0530

    commit

---
+++ Leetcode/SEP_2026/20_09_26.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n = s.size();
+
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            char c = s[i];
+
+            int rev = 26 - (c - 'a');
+            int pos = i + 1;
+
+            sum += rev * pos;
+        }
+        return sum;
+
+    }
+};
